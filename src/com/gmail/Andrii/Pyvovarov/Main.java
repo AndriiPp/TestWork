4ee11c198073bf8015c099dff72ee21bf2bfc114
John Doe <pyvovarov.andrii@gmail.com>
2018-09-19 13:18:51 +0300

    Validation system done

--- src/com/gmail/Andrii/Pyvovarov/Main.java
+++ src/com/gmail/Andrii/Pyvovarov/Main.java
@@ -1,8 +1,19 @@
 package com.gmail.Andrii.Pyvovarov;
 
-public class Main {
+import com.gmail.Andrii.Pyvovarov.util.Parser;
 
+import java.io.IOException;
+
+public class Main {
+    private static final String filePath1 = "C:\\Users\\Lev-\\Desktop\\1.txt";
     public static void main(String[] args) {
 	// write your code here
+        Parser parser = new Parser();
+
+        try {
+            parser.parseFile(filePath1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/gmail/Andrii/Pyvovarov/util/Parser.java
+++ src/com/gmail/Andrii/Pyvovarov/util/Parser.java
@@ -1,8 +1,32 @@
 package com.gmail.Andrii.Pyvovarov.util;
 
-public class Parser {
-private
-    void parseFile(){
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Parser {
+
+
+    public void parseFile(String filePath) throws IOException {
+
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+               String [] linePart =  line.split(Validator.SPACE_SPLITER);
+
+               if(Validator.checkTypeQuestion(linePart[0])){
+
+               }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+
+        }
     }
 }
